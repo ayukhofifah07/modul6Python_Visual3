bd5ccb39a401bf030728064e330236e9858a4295
ayukhofifah07 <ayukhofifah07@gmail.com>
2024-12-04 11:55:31 +0800

    Add files via upload

---
+++ 5.1_where.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    port='3306',
+    database="db_penjualan"
+)
+mycursor = mydb.cursor()
+sql="SELECT * FROM kategori where id = '3'"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
